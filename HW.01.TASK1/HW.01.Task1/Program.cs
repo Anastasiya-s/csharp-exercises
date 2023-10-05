d4569511454321959c7b2ff4482937742054a628
Anastasiya Sukhaverkhaya <ana_stasya@sam-solutions.net>
2023-10-05 10:13:42 +0300

    Solve Array tasks

---
+++ HW.01.TASK1/HW.01.Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+int[] array1 = new int[7];
+int[] array2 = new int[7];
+int[] array3 = new int[7];
+
+Random random = new Random();
+
+for (int i = 0; i < array1.Length; i++) 
+{
+    array1[i] = random.Next();
+}
+for (int i = 0; i < array2.Length; i++)
+{
+    Console.WriteLine("Write a number");
+    int num = Convert.ToInt32(Console.ReadLine());
+    array2[i] = num;
+}
+for (int i = 0; i < array3.Length; i++)
+{
+    array3[i] = array1[i] + array2[i];
+}
+Console.WriteLine(string.Join(", ",array1));
+Console.WriteLine(string.Join(", ",array2));
+Console.WriteLine(string.Join(", ",array3));
+Console.ReadLine();
---
+++ HW.01.Task2/HW.01.Task2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+int[] ints = new int[10];
+
+for (int i = 0; i < ints.Length - 1; i++)
+{
+    Console.WriteLine("Enter array entry");
+    ints[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine("Enter number");
+int lastItem = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter position");
+int lastIndex = Convert.ToInt32(Console.ReadLine());
+
+int[] temp = new int[ints.Length - lastIndex];
+int[] firstPart = new int[ints.Length - temp.Length];
+
+Array.Copy(ints, lastIndex - 1, temp, 0, ints.Length - lastIndex);
+Array.Copy(ints, 0, firstPart, 0, ints.Length - temp.Length);
+
+ints[lastIndex - 1] = lastItem;
+
+foreach (int item in temp)
+{
+    ints[lastIndex + Array.IndexOf(temp, item)] = item;
+}
+
+Console.WriteLine(string.Join(", ", ints));
+Console.ReadLine();
