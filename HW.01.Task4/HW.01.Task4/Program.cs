21e572e04db99c44425455c3ac181361e9a89702
Anastasiya Sukhaverkhaya <ana_stasya@sam-solutions.net>
2023-10-08 11:20:13 +0300

    Solve Task 4

---
+++ HW.01.Task4/HW.01.Task4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+Console.WriteLine("Enter line length");
+string? lineLengthFromConsole = Console.ReadLine();
+bool result = int.TryParse(lineLengthFromConsole, out var lineLength);
+
+for (int i = lineLength; i > 0; i--)
+{
+    if (i != lineLength)
+    {
+        string[] spaces = new string[ lineLength - i];
+        Array.Fill(spaces, " ");
+        int[] numbers = new int[i];
+        Array.Fill(numbers, i);
+        Console.WriteLine($"{string.Join("", spaces)}{string.Join(" ", numbers)}");
+    } else
+    {
+        string[] numbers = new string[i];
+        Array.Fill(numbers, lineLengthFromConsole);
+        Console.WriteLine(string.Join(" ", numbers));
+
+    }
+}
+
+Console.ReadLine();
